44e94482b6f81fc9c10e2ddc8ba448f072a90056
carly <carly@noukod.com>
2019-01-11 16:44:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "My name is Tazz"
+end
+
+get '/hometown' do
+"My hometown is Delmas"
+end
+
+get '/favorite-song' do
+"My favorite song is Peze kafe"
+end
+
 end
